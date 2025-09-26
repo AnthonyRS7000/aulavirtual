2588ca50318fce842e706cd46527109810939bdb
Ingrid Rosales <nahocarlos@gmail.com>
2025-09-26 14:33:17 -0500

    mejora estilos-modos

--- src/features/estudiante/services/horarioService.ts
+++ src/features/estudiante/services/horarioService.ts
@@ -1,6 +1,7 @@
+// horarioService.ts
 import api from "../../../lib/axios";
 
 export const getHorario = async (codigoAlumno: string, semestre: string) => {
   const response = await api.get(`/horario/${codigoAlumno}/${semestre}`);
-  return response.data.data; // solo los cursos
+  return response.data.data;
 };
