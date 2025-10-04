ee681a68ed5d2a4f4bafbb2be6c09c0e1445e0c0
Anthony <thonyrivera9@gmail.com>
2025-10-04 11:23:31 -0500

    cambio de dependencias

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // permite usar @/ para acceder a src/
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@features': path.resolve(__dirname, 'src/features'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@lib': path.resolve(__dirname, 'src/lib'),
-    },
-  },
+      '@': resolve(__dirname, 'src'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@features': resolve(__dirname, 'src/features'),
+      '@hooks': resolve(__dirname, 'src/hooks'),
+      '@lib': resolve(__dirname, 'src/lib')
+    }
+  }
 });
